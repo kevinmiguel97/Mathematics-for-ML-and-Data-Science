86c2736edf08b80bc2507dce1ef6cddee9a45672
Kevin <kevmiguelarronte@gmail.com>
2026-03-31 20:07:26 -0400

    Wk 3 graded lab

--- Linear Algebra/w3/Lab assignment/utils.py
+++ Linear Algebra/w3/Lab assignment/utils.py
@@ -1,42 +1,59 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
+
 def plot_transformation(T, e1, e2):
     color_original = "#129cab"
     color_transformed = "#cc8933"
     
-    _, ax = plt.subplots(figsize=(7, 7))
+    e1 = np.array(e1).reshape(2,)
+    e2 = np.array(e2).reshape(2,)
+    
+    fig, ax = plt.subplots(figsize=(7, 7))
     ax.tick_params(axis='x', labelsize=14)
     ax.tick_params(axis='y', labelsize=14)
-    ax.set_xticks(np.arange(-5, 5))
-    ax.set_yticks(np.arange(-5, 5))
-    
-    plt.axis([-5, 5, -5, 5])
-    plt.quiver([0, 0],[0, 0], [e1[0], e2[0]], [e1[1], e2[1]], color=color_original, angles='xy', scale_units='xy', scale=1)
-    plt.plot([0, e2[0][0], e1[0][0], e1[0][0]], 
-             [0, e2[1][0], e2[1][0], e1[1][0]], 
-             color=color_original)
-    e1_sgn = 0.4 * np.array([[1] if i==0 else [i[0]] for i in np.sign(e1)])
-    ax.text(e1[0]-0.2+e1_sgn[0], e1[1]-0.2+e1_sgn[1], f'$e_1$', fontsize=14, color=color_original)
-    e2_sgn = 0.4 * np.array([[1] if i==0 else [i[0]] for i in np.sign(e2)])
-    ax.text(e2[0]-0.2+e2_sgn[0], e2[1]-0.2+e2_sgn[1], f'$e_2$', fontsize=14, color=color_original)
-    
-    e1_transformed = T(e1)
-    e2_transformed = T(e2)
-    
-    plt.quiver([0, 0],[0, 0], [e1_transformed[0], e2_transformed[0]], [e1_transformed[1], e2_transformed[1]], 
-               color=color_transformed, angles='xy', scale_units='xy', scale=1)
-    plt.plot([0,e2_transformed[0][0], e1_transformed[0][0]+e2_transformed[0][0], e1_transformed[0][0]], 
-             [0,e2_transformed[1][0], e1_transformed[1][0]+e2_transformed[1][0], e1_transformed[1][0]], 
-             color=color_transformed)
-    e1_transformed_sgn = 0.4 * np.array([[1] if i==0 else [i[0]] for i in np.sign(e1_transformed)])
-    ax.text(e1_transformed[0][0]-0.2+e1_transformed_sgn[0][0], e1_transformed[1][0]-e1_transformed_sgn[1][0], 
-            f'$T(e_1)$', fontsize=14, color=color_transformed)
-    e2_transformed_sgn = 0.4 * np.array([[1] if i==0 else [i[0]] for i in np.sign(e2_transformed)])
-    ax.text(e2_transformed[0][0]-0.2+e2_transformed_sgn[0][0], e2_transformed[1][0]-e2_transformed_sgn[1][0], 
-            f'$T(e_2)$', fontsize=14, color=color_transformed)
-    
-    plt.gca().set_aspect("equal")
+    ax.set_xticks(np.arange(-5, 6))
+    ax.set_yticks(np.arange(-5, 6))
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+
+    # Original vectors
+    ax.quiver([0, 0], [0, 0],
+              [e1[0], e2[0]],
+              [e1[1], e2[1]],
+              color=color_original,
+              angles='xy', scale_units='xy', scale=1)
+
+    # Original parallelogram
+    ax.plot([0, e2[0], e1[0] + e2[0], e1[0], 0],
+            [0, e2[1], e1[1] + e2[1], e1[1], 0],
+            color=color_original)
+
+    # Labels
+    ax.text(e1[0] - 0.2, e1[1] - 0.2, '$e_1$', fontsize=14, color=color_original)
+    ax.text(e2[0] - 0.2, e2[1] - 0.2, '$e_2$', fontsize=14, color=color_original)
+
+    # Transformed vectors
+    e1_t = np.array(T(e1)).reshape(2,)
+    e2_t = np.array(T(e2)).reshape(2,)
+
+    ax.quiver([0, 0], [0, 0],
+              [e1_t[0], e2_t[0]],
+              [e1_t[1], e2_t[1]],
+              color=color_transformed,
+              angles='xy', scale_units='xy', scale=1)
+
+    # Transformed parallelogram
+    ax.plot([0, e2_t[0], e1_t[0] + e2_t[0], e1_t[0], 0],
+            [0, e2_t[1], e1_t[1] + e2_t[1], e1_t[1], 0],
+            color=color_transformed)
+
+    # Labels
+    ax.text(e1_t[0] - 0.2, e1_t[1] - 0.2, '$T(e_1)$', fontsize=14, color=color_transformed)
+    ax.text(e2_t[0] - 0.2, e2_t[1] - 0.2, '$T(e_2)$', fontsize=14, color=color_transformed)
+
+    ax.set_aspect("equal")
+    plt.grid(True)
     plt.show()
 
 def initialize_parameters(n_x):
